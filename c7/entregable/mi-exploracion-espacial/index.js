26604b5d1a852f5aa7217d1340618446c8d6561b
Jonathan <jonathan603015@gmail.com>
2026-05-07 11:17:35 -0700

    Proyecto node.js

---
+++ c7/entregable/mi-exploracion-espacial/index.js
@@ -0,0 +1,8 @@
+const planetas = require('./planetas');
+
+planetas.forEach(planeta => {
+    console.log(`Planeta ${planeta.nombre} descubierto..!`);
+    console.log(`Descripcion: ${planeta.descripcion}`);
+    console.log(`Descubierto en ${planeta.descubiertoEn}`);
+    console.log("--------------------------");
+})
---
+++ c7/entregable/mi-exploracion-espacial/planetas.js
@@ -0,0 +1,29 @@
+const planetas = [
+  {
+    nombre: "Titán",
+    descripcion: "La luna más grande de Saturno, con lagos de metano.",
+    descubiertoEn: "1655"
+  },
+  {
+    nombre: "Próxima Centauri b",
+    descripcion: "Un exoplaneta rocoso en la zona habitable de su estrella.",
+    descubiertoEn: "2016"
+  },
+  {
+    nombre: "Marte",
+    descripcion: "Conocido como el planeta rojo, tiene el volcán más grande del sistema solar.",
+    descubiertoEn: "Antigüedad"
+  },
+  {
+    nombre: "Kepler-452b",
+    descripcion: "A menudo llamado el 'primo de la Tierra' debido a su tamaño y órbita similares.",
+    descubiertoEn: "2015"
+  },
+  {
+    nombre: "Júpiter",
+    descripcion: "El planeta más grande del sistema solar, una gigante gaseosa con una Gran Mancha Roja.",
+    descubiertoEn: "Antigüedad"
+  }
+];
+
+module.exports = planetas;
